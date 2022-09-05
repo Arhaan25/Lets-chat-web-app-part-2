f42be439c17deb90b6b1cca21be04bc6b94daa63
Arhaan25 <101710365+Arhaan25@users.noreply.github.com>
2022-09-05 15:35:17 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCWa0exfSQzH-7RcCrnXfoTMiQlQpsv3NQ",
+    authDomain: "lets-chat-web-app-b8cc6.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-b8cc6-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-b8cc6",
+    storageBucket: "lets-chat-web-app-b8cc6.appspot.com",
+    messagingSenderId: "183638284916",
+    appId: "1:183638284916:web:92e2b8ba8458b7e4170f21"
+  };
+firebase.initializeApp(firebaseConfig);
